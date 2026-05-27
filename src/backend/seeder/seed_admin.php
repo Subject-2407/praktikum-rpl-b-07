d5b86f4c8dab69296b8155d1a9f19693df771156
Subject-2407 <stevanputra.bintang@gmail.com>
2026-05-27 19:55:29 +0700

    fix(seeder): fix require_once path

--- src/backend/seeder/seed_admin.php
+++ src/backend/seeder/seed_admin.php
@@ -13,8 +13,8 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/config/bootstrap.php';
+require_once __DIR__ . '\..\vendor\autoload.php';
+require_once __DIR__ . '\..\config\bootstrap.php';
 
 use Scapes\Infrastructure\Database\DatabaseConnection;
 
