8e9238715e3f8665d88ac8c4b48e8751f11fd218
allyssahprw <wannastillawake00@gmail.com>
2026-06-06 08:08:23 +0700

    feat: update portal.js

---
+++ src/frontend/contributor/scripts/portal.js
@@ -0,0 +1,7 @@
+import { guardPortal } from './presentation/pages/settings-page.js';
+import { initPortalLayout } from './presentation/layouts/portal-layout.js';
+import { initSettingsPage } from './presentation/pages/settings-page.js';
+
+guardPortal();
+initPortalLayout();
+initSettingsPage();
