862d0f10824f9fc766d32c7b3212d6168833ed5d
Subject-2407 <stevanputra.bintang@gmail.com>
2026-06-10 05:47:39 +0700

    chore(backend): add structured error logging for controller failures

--- src/backend/src/Interfaces/Http/Controllers/ModerationController.php
+++ src/backend/src/Interfaces/Http/Controllers/ModerationController.php
@@ -19,6 +19,7 @@ use Scapes\Application\UseCases\Moderation\ModerateWallpaperUseCase;
 use Scapes\Core\Exceptions\NotFoundException;
 use Scapes\Core\Exceptions\UnprocessableEntityException;
 use Scapes\Core\Exceptions\ValidationException;
+use Scapes\Infrastructure\Logging\AppLogger;
 use Scapes\Interfaces\Http\Request;
 use Scapes\Interfaces\Http\Resources\WallpaperResource;
 use Scapes\Interfaces\Http\Response;
@@ -82,7 +83,9 @@ class ModerationController {
         $result['meta']
       );
     } catch (\Throwable $e) {
-      return $this->handleException($e);
+      return $this->handleException($e, 'index', [
+        'query' => $query,
+      ]);
     }
   }
 
@@ -116,7 +119,11 @@ class ModerationController {
         WallpaperResource::moderated($wallpaper, Request::baseUrl())
       );
     } catch (\Throwable $e) {
-      return $this->handleException($e);
+      return $this->handleException($e, 'update', [
+        'wallpaper_id' => $id,
+        'payload' => $data,
+        'auth_user' => $authUser,
+      ]);
     }
   }
 
@@ -127,7 +134,11 @@ class ModerationController {
    *
    * @return array<string, mixed>
    */
-  private function handleException(\Throwable $e): array {
+  private function handleException(
+    \Throwable $e,
+    string $action = 'unknown',
+    array $context = []
+  ): array {
     if ($e instanceof ValidationException) {
       return Response::error('Validation failed.', 400, $e->getErrors());
     }
@@ -139,6 +150,14 @@ class ModerationController {
     if ($e instanceof UnprocessableEntityException) {
       return Response::error($e->getMessage(), 422);
     }
+
+    AppLogger::logThrowable('moderation_controller_exception', $e, array_merge(
+      [
+        'controller' => self::class,
+        'action' => $action,
+      ],
+      $context
+    ));
 
     return Response::error('Internal server error.', 500);
   }
