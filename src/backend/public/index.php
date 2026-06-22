8f3c87530912a0cb8cb9b157df3f906c85430b18
Subject-2407 <stevanputra.bintang@gmail.com>
2026-06-23 01:44:39 +0700

    feat: allow PUT method

--- src/backend/public/index.php
+++ src/backend/public/index.php
@@ -52,7 +52,7 @@ if (in_array($origin, $allowedOrigins, true)) {
 }
 
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Accept, X-Requested-With, X-CSRF-Token');
 header('Access-Control-Max-Age: 86400');
 
